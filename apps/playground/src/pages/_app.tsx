607ace3c1bd2d9ea7e97f14df135d8281b31e7cf
Yasuyuki Takeo <yasuflatland@gmail.com>
2025-05-11 12:44:24 +0900

    chore: Add sidebar initial

--- apps/playground/src/pages/_app.tsx
+++ apps/playground/src/pages/_app.tsx
@@ -1,6 +1,20 @@
 import "@/styles/globals.css";
+import { AppSidebar } from "@/components/app-sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<SidebarProvider>
+			<Head>
+				<title>Playground</title>
+			</Head>
+			<AppSidebar />
+			<main>
+				<SidebarTrigger />
+				<Component {...pageProps} />
+			</main>
+		</SidebarProvider>
+	);
 }
